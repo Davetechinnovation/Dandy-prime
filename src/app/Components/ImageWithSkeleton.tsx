05d0706f1cb0b2568111bccd37d69afe70d2ca73
Davetechinnovation <davetechinnovation@gmail.com>
2025-07-05 00:53:17 +0100

    massive update enjoy

--- src/app/Components/ImageWithSkeleton.tsx
+++ src/app/Components/ImageWithSkeleton.tsx
@@ -2,25 +2,25 @@ import React, { useState } from "react";
 import Image, { ImageProps } from "next/image";
 import Skeleton from "./skeleton";
 
-const ImageWithSkeleton: React.FC<ImageProps> = ({ ...props }) => {
-  const [imgLoaded, setImgLoaded] = useState(false);
-  const [imgError, setImgError] = useState(false);
+const ImageWithSkeleton: React.FC<ImageProps> = (props) => {
+  const [imgLoaded, setImgLoaded] = useState(false);
+  const [imgError, setImgError] = useState(false);
 
-  if (imgError) {
-    return <Skeleton />;
-  }
-
-  return (
-    <>
-      {!imgLoaded && <Skeleton />}
-      <Image
-        {...props}
-        onLoad={() => setImgLoaded(true)}
-        onError={() => setImgError(true)}
-        style={{ display: imgLoaded ? "block" : "none" }}
-      />
-    </>
-  );
+  return (
+    <div className="relative w-full h-[120px] sm:h-[250px] rounded-t-lg overflow-hidden">
+      {!imgLoaded && <Skeleton />}
+      {!imgError && (
+        <Image
+          {...props}
+          onLoad={() => setImgLoaded(true)}
+          onError={() => setImgError(true)}
+          className={`${props.className} absolute top-0 left-0 w-full h-full object-cover transition-opacity duration-500 ${
+            imgLoaded ? "opacity-100" : "opacity-0"
+          }`}
+        />
+      )}
+    </div>
+  );
 };
 
 export default ImageWithSkeleton;
