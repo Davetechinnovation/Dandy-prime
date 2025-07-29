f6c5293777b187229156b2761a2438f3e6ce2641
Davetechinnovation <davetechinnovation@gmail.com>
2025-07-29 14:00:24 +0100

    update

--- src/app/detail/page.tsx
+++ src/app/detail/page.tsx
@@ -1,20 +1,19 @@
-'use client';
+"use client";
 import {
   AlertTriangleIcon,
   ArrowLeft,
   Bookmark,
   Download,
   Play,
+  PlayCircle,
+  PlayCircleIcon,
   Share2,
   Star,
+  StarIcon,
 } from "lucide-react";
 import Image from "next/image";
 import React from "react";
-import { useRouter } from 'next/navigation';
-
-
-
-
+import { useRouter } from "next/navigation";
 
 const Page = () => {
   const router = useRouter();
@@ -37,6 +36,9 @@ const Page = () => {
             unoptimized
             className="object-cover"
           />
+          <p className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  ">
+            <PlayCircleIcon className="w-10 h-10 fill-blue-700 cursor-pointer " />
+          </p>
           <div className="max-w-[150px]  small:hidden h-[220px] absolute w-full -bottom-[70px] left-10 ">
             <Image
               src="/images/test.webp"
@@ -46,7 +48,7 @@ const Page = () => {
               className="object-cover rounded-lg "
             />
           </div>
-            <p
+          <p
             onClick={handleBack}
             className="md:px-2 px-1 sm:py-3 py-1 absolute top-0 left-0 cursor-pointer border-2 border-blue-700 rounded-xl"
           >
@@ -103,8 +105,11 @@ const Page = () => {
             </div>
           </div>
           <div className="grid grid-cols-2 pt-6 gap-3">
-            <button className="bg-blue-700 rounded-full sm:py-3 py-2 ">
-              Watch Now
+            <button className="bg-blue-700 rounded-full sm:py-3 py-2 flex items-center gap-2 justify-center hover:bg-transparent border-2 border-blue-700 duration-500 transition-all hover:text-blue-700  ">
+              <span>
+                <PlayCircle />
+              </span>
+              Watch Trailer
             </button>
             <select
               name="server"
@@ -119,6 +124,64 @@ const Page = () => {
               <option value="6">Server 6</option>
               <option value="7">Server 7</option>
             </select>
+          </div>
+        </div>
+      </div>
+
+      <div className="sm:pt-[78px] px-4 ">
+        <div className="flex gap-7">
+          <div className="">
+            <h2 className="text-[25px] font-semibold">8.5</h2>
+            <p className="flex items-center gap-1">
+              <StarIcon className="w-4 h-4 fill-yellow-400 text-yellow-400 " />{" "}
+              <StarIcon className="w-4 h-4 fill-yellow-400 text-yellow-400 " />{" "}
+              <StarIcon className="w-4 h-4 fill-yellow-400 text-yellow-400 " />{" "}
+              <StarIcon className="w-4 h-4 fill-yellow-400 text-yellow-400 " />{" "}
+              <StarIcon className="w-4 h-4 fill-yellow-400 text-yellow-400 " />
+            </p>
+            <p>57625 (reviews)</p>
+          </div>
+
+          <div>
+            <div className="flex items-center gap-1">
+              <p>5</p>
+              <p className="w-[260px] h-[10px] relative  rounded-full bg-[#cecece] ">
+                <span className="w-[75%] rounded-full h-full bg-blue-700 absolute inset-0 "></span>
+              </p>
+              <p>75%</p>
+            </div>
+
+            <div className="flex items-center gap-1">
+              <p>4</p>
+              <p className="w-[260px] h-[10px] relative  rounded-full bg-[#cecece] ">
+                <span className="w-[20%] rounded-full h-full bg-blue-700 absolute inset-0 "></span>
+              </p>
+              <p>20%</p>
+            </div>
+
+            <div className="flex items-center gap-1">
+              <p>3</p>
+              <p className="w-[260px] h-[10px] relative  rounded-full bg-[#cecece] ">
+                <span className="w-[10%] rounded-full h-full bg-blue-700 absolute inset-0 "></span>
+              </p>
+              <p>10%</p>
+            </div>
+
+            <div className="flex items-center gap-1">
+              <p>2</p>
+              <p className="w-[260px] h-[10px] relative  rounded-full bg-[#cecece] ">
+                <span className="w-[5%] rounded-full h-full bg-blue-700 absolute inset-0 "></span>
+              </p>
+              <p>5%</p>
+            </div>
+
+            <div className="flex items-center gap-1">
+              <p>1</p>
+              <p className="w-[260px] h-[10px] relative  rounded-full bg-[#cecece] ">
+                <span className="w-[2%] rounded-full h-full bg-blue-700 absolute inset-0 "></span>
+              </p>
+              <p>2%</p>
+            </div>
           </div>
         </div>
       </div>
